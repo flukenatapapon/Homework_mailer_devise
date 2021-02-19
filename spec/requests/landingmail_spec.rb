cf4b708af75d4098af9e27dc4ea403d405f05186
natapapon <natapapon.r@gmail.com>
2021-02-19 14:07:59 +0700

    Add landing mail request_spec

---
+++ spec/requests/landingmail_spec.rb
@@ -0,0 +1,18 @@
+require 'rails_helper'
+
+RSpec.describe "Landingmail", type: :request do
+
+    let(:landingmail_params) {
+        { email: "natapapon.r@gmail.com", message: "This is a test mail" }
+    } 
+
+    describe "POST /send_mail" do
+        it "send email successfully" do
+        expect { 
+                post send_mail_url, 
+                params: { landingmail_params }
+            }.to change{ ActionMailer::Base.deliveries.count }.by(1)
+        end
+    end
+    
+end
---
+++ test/controllers/landingmail_controller_test.rb
@@ -0,0 +1,7 @@
+require "test_helper"
+
+class LandingmailController < ActionDispatch::IntegrationTest
+  # test "the truth" do
+  #   assert true
+  # end
+end
